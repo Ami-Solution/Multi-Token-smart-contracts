2707eb2cbe8211bd5f3aa19fc0f088b6780f674c
Natalie Chin <chinnh@mcmaster.ca>
2018-07-13 14:02:18 -0400

    Updated STKToken tests to match embark framework

--- test/STKToken.js
+++ test/STKToken.js
@@ -1,16 +1,83 @@
-var STKToken = artifacts.require("./STKToken.sol");
+/*global contract, config, it, assert, web3*/
+const STKChannel = require('Embark/contracts/STKChannel');
+const ERC20Token = require('Embark/contracts/ERC20Token');
+const STKLibrary = require('Embark/contracts/STKLibrary');
+const indexes = require('./helpers/ChannelDataIndexes.js')
+const StandardToken = require('Embark/contracts/StandardToken.sol');
+const Token = require('Embark/contracts/Token.sol');
 
-contract('STKToken', accounts => {
-  userAccount = accounts[0] ;
-  it("Should have 1 billion tokens in first account", async()=> {
-      const instance = await STKToken.deployed();
-      const balance = await instance.balanceOf(userAccount);
-      assert.equal(balance.valueOf(), 1000000000, '1 billion was not in the first account');
-  });
 
-  it('Should have symbol as STK',async()=> {
-      const instance = await STKToken.deployed();
-      const symbol = await instance.symbol.call();
-      assert.equal(symbol,'STK','Symbol is not STK');
+contract("Testing ERC20 Token", function () {
+    this.timeout(0);
+    let allAccounts;
+    const timeout = 10;
+    const initialCreation = 1000000000; 
+
+    before(function(done) {
+        web3.eth.getAccounts(function (err, accounts) {
+            if (err) {
+                return done(err);
+            }
+            allAccounts = accounts;
+
+            config({
+                contracts: {
+                    "Token": {
+
+                    },
+                    "StandardToken": {
+
+                    },
+                    "ERC20Token": {
+                        args: [initialCreation,'STK', 18, 'STK'],
+                        "fromIndex":3
+                    },
+                    STKLibrary: {
+                        args: [
+                            '$ERC20Token',
+                            accounts[0],
+                            accounts[2],
+                            accounts[1],
+                            timeout,
+                            1,
+                            0,
+                            0
+                        ],
+                        "fromIndex":1
+                    },
+                    "STKChannel": {
+                        args: [
+                            accounts[0],
+                            accounts[2],
+                            '$ERC20Token',
+                            timeout
+                        ],
+                        "fromIndex":1
+                    }
+                }
+            }, done);
+        });
     });
+
+    it("STK Token is deployed ", function()
+    {
+        let ERC20TokenAddress = ERC20Token.options.address;
+        assert.ok(ERC20TokenAddress.length > 0);
+    });
+
+    it("STK Token should have 1 billion tokens in initialized account after declaration", async()=> {
+        const balance = await ERC20Token.methods.balanceOf(allAccounts[3]).call(); 
+        assert.equal(balance.valueOf(), 1000000000, '1 billion was not in the first account');
+    });
+    
+    it('STK Token should have symbol as STK',async()=> {
+        const symbol = await ERC20Token.symbol.call();
+        assert.equal(symbol,'STK','Symbol is not STK');
+    });  
+    
+    it('STK Token should have 18 decimal points', async() => { 
+        const decimal = await ERC20Token.decimals.call(); 
+        assert.equal(decimal, 18, "Decimal points is not 18"); 
+    })
+    
 });
